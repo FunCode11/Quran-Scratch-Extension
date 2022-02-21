5c1ddb23f6635fb452591340f2f8d3ae78e6ed66
FunCode11 <96876308+FunCode11@users.noreply.github.com>
2022-02-21 13:16:46 -0500

    Update and rename qurantranslation.js to qurantranslator.js

--- qurantranslator.js
+++ qurantranslator.js
@@ -23,10 +23,13 @@ class Quran {
     };
   }
   quran(args) {
-    // Note strict equality: Inputs must match exactly: in type, case, etc.
-    return fetch(`http://api.alquran.cloud/v1/ayah/${args.SURAH}:${args.AYAH}/en.asad`)
+    let result;
+    fetch(`http://api.alquran.cloud/v1/ayah/${args.SURAH}:${args.AYAH}/en.asad`)
     .then(res => res.json())
-    .then(data => data.data.text);
+    .then(data => {
+      result = data.data.text
+    });
+    return result
   }
 }
 Scratch.extensions.register(new Quran());
